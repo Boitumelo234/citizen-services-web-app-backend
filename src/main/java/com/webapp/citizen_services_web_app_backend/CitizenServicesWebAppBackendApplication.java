b24b56da3998aa72c9e0d736ea4e0f31cc370562
Boitumelo234 <220604586@tut4life.ac.za>
2026-02-23 16:31:30 +0200

    The message

---
+++ src/main/java/com/webapp/citizen_services_web_app_backend/CitizenServicesWebAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.citizen_services_web_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitizenServicesWebAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitizenServicesWebAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webapp/citizen_services_web_app_backend/CitizenServicesWebAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.citizen_services_web_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitizenServicesWebAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
